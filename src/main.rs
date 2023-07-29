c775df6cd9bd711d93e3cbd2b6a1c66aa23a6743
pedroafleite <pedroaug.fl@gmail.com>
2023-07-29 16:24:14 -0300

    added wdbdk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
     println!("Hello, world!");
+    dotenv::from_filename(".env").ok();
+
+    let descriptor = env::var("WALLET_DESCRIPTOR").unwrap();  
+
+    println!("Descriptor: {}", descriptor);
+    dbg!(descriptor);
+
 }
