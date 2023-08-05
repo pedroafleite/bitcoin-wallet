6ba49834d0f12405789ec3afc37a37f49efa6825
pedroafleite <pedroaug.fl@gmail.com>
2023-08-05 10:42:48 -0300

    merged codes and compiled

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,31 @@
-// use bdk::{bitcoin::Network, database::SqliteDatabase, Wallet};
-// use std::path::Path;
-
-// fn setup() -> Result<String, Box<dyn std::error::Error>> {
-//     dotenv::from_filename(".env").ok();
-//     let descriptor = std::env::var("WALLET_DESCRIPTOR")?;
-//     Ok(descriptor)
-// }
-
-// fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let descriptor = setup()?;
-
-//     let my_path: &Path = Path::new("bitcoin_wallet.db");
-
-//     let _wallet: Result<Wallet<SqliteDatabase>, bdk::Error> = Wallet::new(
-//         &descriptor,
-//         None,
-//         Network::Testnet,
-//         SqliteDatabase::new(&my_path),
-//     );
-
-//     let address: Result<bdk::wallet::AddressInfo, bdk::Error> =
-//         _wallet?.get_address(bdk::wallet::AddressIndex::New);
-//     dbg!(address)?;
-
-//     Ok(())
-// }
-
 use axum::{response::Html, routing::get, Router};
+use bdk::{bitcoin::Network, database::SqliteDatabase, Wallet};
 use std::net::SocketAddr;
+use std::path::Path;
+
+fn setup() -> Result<String, Box<dyn std::error::Error>> {
+    dotenv::from_filename(".env").ok();
+    let descriptor = std::env::var("WALLET_DESCRIPTOR")?;
+    Ok(descriptor)
+}
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let descriptor = setup()?;
+
+    let my_path: &Path = Path::new("bitcoin_wallet.db");
+
+    let _wallet: Result<Wallet<SqliteDatabase>, bdk::Error> = Wallet::new(
+        &descriptor,
+        None,
+        Network::Testnet,
+        SqliteDatabase::new(&my_path),
+    );
+
+    let address: Result<bdk::wallet::AddressInfo, bdk::Error> =
+        _wallet?.get_address(bdk::wallet::AddressIndex::New);
+    dbg!(address)?;
+
     // build our application with a route
     let app = Router::new().route("/", get(handler));
 
@@ -45,7 +40,13 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
+
+// async fn main() {
+
+// }
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
